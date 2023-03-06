87f33dfd14ea7f964cda4aef770978930c380d15
Abolas <postmaster@matthieu-griffon.fr>
2023-03-06 23:10:18 +0100

    Changes to be committed: 	modified:   apps.js 	new file:   helpers.js 	new file:   mock-pokemon.js 	modified:   note.txt

--- apps.js
+++ apps.js
@@ -1,15 +1,23 @@
 const express = require('express')
+const {success} = require('./helpers.js')//Ici on importe que la méthode success du helpers.js
+let pokemons = require('./mock-pokemon')
 
 const app = express()
 const port = 3000
 
 app.get ('/', (req,res) => res.send('Hello again again , Express 2!!'))
-
-app.get('/api/pokemons/:id/:name/:type', (req, res)=> {
-    const id= req.params.id
-    const name = req.params.name
-    const type = req.params.type
-    res.send(`Vous avez demandé le pokémon n°${id} dont le nom est ${name} et le types est ${type}.`)
+// On retourne la liste des pokémons au format Json, avec un message :
+app.get ('/api/pokemons', (req, res) => {
+    const message = 'La liste des pokemons a bien été récuperée.'
+    res.json (success(message, pokemons))//vraiment tout con
 })
+app.get('/api/pokemons/:id', (req, res)=> {
+    const id= parseInt(req.params.id)
+    const pokemon = pokemons.find(pokemon => pokemon.id === id)
+    const message = 'Un pokémon a bien été trouvé.'
+    res.json(success(message, pokemon))//On appeler la méthode success car on la importé directement du helpers.js grace a const{success} ligne 2.
+})
+
+
 
 app.listen(port, () => console.log(`Notre application Node est démarrée sur : htt://localhost:${port}`))
